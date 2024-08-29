5987f4ad25f4d171e6ef81d0eb81175c43346908
eli-crow <emcrow@sep.com>
2024-08-28 22:07:57 -0400

    Allow erasing terrain tiles with shift key

--- src/editor/tools/TerrainTileTool.ts
+++ src/editor/tools/TerrainTileTool.ts
@@ -8,16 +8,6 @@ type ToolState = { type: "idle" } | { type: "down" } | { type: "dragging" };
 
 export class TerrainTileTool extends Tool<SupportsTerrainTileTool> {
   #state: ToolState = { type: "idle" };
-  #erase: boolean = false;
-
-  get erase() {
-    return this.#erase;
-  }
-
-  set erase(value: boolean) {
-    this.#erase = value;
-    this.notifyChanged();
-  }
 
   onPointerDown(x: number, y: number, event: PointerEvent) {
     if (event.button !== 0) {
@@ -25,17 +15,18 @@ export class TerrainTileTool extends Tool<SupportsTerrainTileTool> {
     }
 
     if (this.#state.type === "idle") {
+      const shouldErase = event.shiftKey;
       const position = this.tileset.getTilePositionAtPixel(x, y);
       if (!position) {
         return;
       }
-      this.tileset.setTile(position.x, position.y, !this.erase);
+      this.tileset.setTile(position.x, position.y, !shouldErase);
       this.tileset.invalidate();
       this.#state = { type: "down" };
     }
   }
 
-  onPointerMove(x: number, y: number) {
+  onPointerMove(x: number, y: number, event: PointerEvent) {
     if (this.#state.type === "idle") {
       return;
     }
@@ -45,16 +36,17 @@ export class TerrainTileTool extends Tool<SupportsTerrainTileTool> {
     }
 
     if (this.#state.type === "dragging") {
+      const shouldErase = event.shiftKey;
       const position = this.tileset.getTilePositionAtPixel(x, y);
       if (!position) {
         return;
       }
-      this.tileset.setTile(position.x, position.y, !this.erase);
+      this.tileset.setTile(position.x, position.y, !shouldErase);
       this.tileset.invalidate();
     }
   }
 
-  onPointerUp(_x: number, _y: number) {
+  onPointerUp(_x: number, _y: number, _event: PointerEvent) {
     this.#state = { type: "idle" };
   }
 }
