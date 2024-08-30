bbbb854c2d8120680cd7e0d722ad30efe7e4138e
eli-crow <emcrow@sep.com>
2024-08-29 23:23:19 -0400

    Update back button style

--- src/ui/views/TilesetEditorTopbar.tsx
+++ src/ui/views/TilesetEditorTopbar.tsx
@@ -1,4 +1,5 @@
 import { mergeClasses } from "../../utilities";
+import BackIcon from "../icons/back.svg?react";
 import { useTilesetEditorPageContext } from "../providers/TilesetEditorPageProvider";
 import classes from "./TilesetEditorTopbar.module.css";
 
@@ -6,7 +7,9 @@ export function TilesetEditorTopbar() {
   const page = useTilesetEditorPageContext();
   return (
     <div className={mergeClasses(classes.root, "surface-translucent")}>
-      <button className={classes.back}>{"<"}</button>
+      <button className={classes.back}>
+        <BackIcon />
+      </button>
       <h1 className={classes.title}>{page.tilesetName}</h1>
       <div className={classes.modeGroup}>
         <button className={classes.mode} aria-current={page.mode === "raw"} onClick={() => page.setMode("raw")}>
